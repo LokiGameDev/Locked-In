6f991027fcd27674b69f2d8f0b3717655fa6ce2c
LokiGameDev <lokeshwarank007@gmail.com>
2025-01-14 09:45:55 +0530

    settings update commit
    added graphics , fullscreen , volume settings to game

--- Assets/Scripts/Managers/SettingsManager.cs
+++ Assets/Scripts/Managers/SettingsManager.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] individualSettings;
+    private int _activeSettingIndex;
+    public AudioMixer audioMixer;
+
+    // Fullscreen enable/disable function
+    public void SetFullScreen(bool isFullScreenOn)
     {
-        
+        Screen.fullScreen=isFullScreenOn;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Individual settings panel activator
+    public void IndividualSettingMenu(int index)
     {
-        
+        individualSettings[_activeSettingIndex].SetActive(false);
+        _activeSettingIndex=index;
+        individualSettings[index].SetActive(true);
+    }
+
+    // Volume level controller
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume",volume);
+    }
+
+    // Quality setting function
+    public void SetQualityLevel(int qualityLevel)
+    {
+        QualitySettings.SetQualityLevel(qualityLevel);
     }
 }
