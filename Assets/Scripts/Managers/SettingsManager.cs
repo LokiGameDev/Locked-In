584bbe03334f13449c810d9aa4e0c623a5d474e7
LokiGameDev <lokeshwarank007@gmail.com>
2025-01-16 09:53:02 +0530

    ui update commit
    added resolution option with available screen options

--- Assets/Scripts/Managers/SettingsManager.cs
+++ Assets/Scripts/Managers/SettingsManager.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class SettingsManager : MonoBehaviour
 {
     public GameObject[] individualSettings;
+    
+    public TMP_Dropdown resolutionDropdown;
     private int _activeSettingIndex;
     public AudioMixer audioMixer;
+    public Resolution[] resolutions;
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> resOptions = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for(int i=0;i<resolutions.Length;i++)
+        {
+            string option = resolutions[i].width + "X" + resolutions[i].height;
+            resOptions.Add(option);
+
+            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(resOptions);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     // Fullscreen enable/disable function
     public void SetFullScreen(bool isFullScreenOn)
     {
         Screen.fullScreen=isFullScreenOn;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = resolutions[resolutionIndex];
+        Screen.SetResolution(res.width , res.height , Screen.fullScreen);
     }
 
     // Individual settings panel activator
